99af9f32fb383899ea5f09e26cd52efe68130683
yoshino-nya <wu_yi_@outlook.com>
2026-09-25 00:30:06 +0800

    chore: 增加文章信息与 sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,25 @@ import { unified } from '@astrojs/markdown-remark';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    markdown: {
-        processor: unified({
-            remarkPlugins: [remarkMath],
-            rehypePlugins: [rehypeKatex],
-        }),
-        shikiConfig: {
-            themes: {
-                light: 'github-light',
-                dark: 'github-dark',
-            },
-            wrap: false,
-        },
-    },
+  site: "https://blog.wuyi.host",
+
+  markdown: {
+      processor: unified({
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+      }),
+      shikiConfig: {
+          themes: {
+              light: 'github-light',
+              dark: 'github-dark',
+          },
+          wrap: false,
+      },
+  },
+
+  integrations: [sitemap()],
 });
